7755787097f081df3d5f408806d6281048c2f625
Shikha Rai <shikha.rai1@BLRETV-G17TVQVL.local>
2023-09-12 15:04:32 +0530

    my first commit

---
+++ Demo.java
@@ -0,0 +1,10 @@
+
+// Java Program to demonstrate the
+// syntax of the main() function
+ 
+class GeeksforGeeks {
+    public static void main(String[] args)
+    {
+        System.out.println("I am a Geek");
+    }
+}
